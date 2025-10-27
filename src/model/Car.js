df34eace6e87fc624de8e9d621d1774387b6b2ad
yundoll <mar0722@naver.com>
2025-10-27 18:31:30 +0900

    feat(model): 경주 수행 로직 구현
    - Car 클래스에서 랜덤값을 기반으로 조건부로 위치를 이동시키는 move 함수 구현
    - RacingGame 클래스에서 참가한 자동차들을 move 시키고, 현재 위치를 반환시키는 playRound 함수 구현
    - playRound 함수 반환 시 이름도 함께 반환시키기 위해 Car 클래스에 getName 함수 구현

--- src/model/Car.js
+++ src/model/Car.js
@@ -1,10 +1,19 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Car {
   constructor(name) {
     this.name = name;
     this.position = 0;
   }
 
-  move() {}
+  move() {
+    const randNum = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (4 <= randNum) this.position++;
+  }
+
+  getName() {
+    return this.name;
+  }
   getPosition() {
     return this.position;
   }
--- src/model/RacingGame.js
+++ src/model/RacingGame.js
@@ -1,11 +1,20 @@
-import Car from "./Car";
+import Car from "./Car.js";
 
 class RacingGame {
   constructor(carNames) {
     this.cars = carNames.map((name) => new Car(name));
   }
 
-  playRound() {}
+  playRound() {
+    this.cars.forEach((car) => {
+      car.move();
+    });
+
+    return this.cars.map((car) => ({
+      name: car.getName(),
+      position: car.getPosition(),
+    }));
+  }
   getWinners() {}
 }
 
